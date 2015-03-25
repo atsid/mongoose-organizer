a47809089e3b04cb343c26454febcacf6fc25c4d
Chris Trevino <chris.trevino@atsid.com>
2015-03-24 18:00:48 -0700

    Adding verification to tests

--- src/index.test.js
+++ src/index.test.js
@@ -5,6 +5,9 @@ let expect = chai.expect;
 describe("The Mongoose Organizer", () => {
     it('can autowire a new model', () => {
         let User = require('../test/User');
-        expect(User).to.be.ok;
+        expect(User).to.exist.and.to.be.a.function;
+        expect(User.modelName).to.equal("User");
+        expect(User.schema.methods.isValidPassword).to.be.a.function;
+        expect(User.schema.virtuals.emailReverse.path).to.equal('emailReverse');
     });
 });
